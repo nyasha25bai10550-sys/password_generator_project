642c26f1cb98a26baf5e4b8389cae5e5f43a84f5
nyasha25bai10550-sys <nyasha.25bai10550@vitbhopal.ac.in>
2025-11-23 19:05:45 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,18 +1,9 @@
 import random
 import string
-
-# --- Function jo password banata hai ---
 def generate_password(length):
-    # Saare characters ek jagah: ABC...abc..., 0-9, @#$% etc.
     characters = string.ascii_letters + string.digits + string.punctuation
-    
-    # Random characters ko jod kar password banta hai
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
+length = int(input("how many characters do you want in your password? "))
 
-
-# --- User se length poochna ---
-length = int(input("Kitne characters ka password chahiye? "))
-
-# --- Final output ---
-print("Yeh lo tumhara secure password:", generate_password(length))
+print("your password is ready:", generate_password(length))
